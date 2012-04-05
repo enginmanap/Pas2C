7fa4a4adedc16199f7dd66d3f4b9003badb050b3
Engin Manap <enginmanap@gmail.com>
2012-04-06 00:07:38 +0300

    Add sample input file for testing.

---
+++ sample_input.pas
@@ -0,0 +1,12 @@
+program testprogram;
+begin
+    variable1 : integer;
+    variable2 : integer;
+
+    variable2 := 40 / 3;
+    begin
+	variable1 := variable2 + 5 * 40;
+    end;
+
+    writeln("hello world! \n");
+end.
