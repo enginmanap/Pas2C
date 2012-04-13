f6288b0c11c57568a3fbf7aa6b45bf78700af9b9
Engin Manap <enginmanap@gmail.com>
2012-04-13 21:04:11 +0300

    Add support for underscore in variable names.

--- sample_input.pas
+++ sample_input.pas
@@ -3,18 +3,18 @@ const
     const1 = 5;
     const2 = 7;
 var
-    variable1 : integer;
+    variable_1 : integer;
     variable2, variable3,variable15 : integer;
 begin
 
     variable2 := 40 / 3;
     begin
-	variable1 := variable2 + 5 * 40;
+	variable_1 := variable2 + 5 * 40;
     end;
 
     writeln('hello world!');
-    writeln('first variable is:', variable1, ', and second variable is:', variable2, 'const1 is:', const1);
+    writeln('first variable is:', variable_1, ', and second variable is:', variable2, 'const1 is:', const1);
     writeln('We can print this way too!');
-    write('first variable is:', variable1);
+    write('first variable is:', variable_1);
     writeln(', and second variable is:', variable2);
 end.
